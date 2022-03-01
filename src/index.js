5e7ff2db359a304a55d2ba605106bfb2a20dcc3d
dev-mahmoudwahba <devmahmoudwahba@gmail.com>
2022-03-01 15:37:48 +0200

    video #4

--- src/index.js
+++ src/index.js
@@ -1,20 +1,45 @@
-let toggle = false
-var toggleText = () => {
-    toggle = !toggle
-    render()
-}
-function render() {
-    let content = (
-        <div>
-                <button onClick = {toggleText}>toggle text</button>
-                {toggle ? <p>this is text</p> : ""}
-                
-        </div>
-    
-    )
-    
-    ReactDOM.render(content, document.getElementById("app"));
+class App extends React.Component {
+  render() {
+    return <div className="app">my app
+        <Header/>
+        <ListItems/>
+        <AddItem/>
+    </div>;
+  }
 }
 
+class Header extends React.Component {
+  render() {
+    return <header>header </header>;
+  }
+}
+class ListItems extends React.Component {
+  render() {
+    return <div> list item component
 
-render()
+            <Item/>
+            <Item/>
+            <Item/>
+
+    </div>;
+  }
+}
+
+class Item extends React.Component {
+  render() {
+    return <div>item</div>;
+  }
+}
+
+class AddItem extends React.Component {
+  render() {
+    return (
+      <form>
+        <input type="text" />
+        <input type="submit" />
+      </form>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
