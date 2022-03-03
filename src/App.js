978e52a36eb2b6a93cb73c74802d54a7b1099efc
dev-mahmoudwahba <devmahmoudwahba@gmail.com>
2022-03-03 17:50:13 +0200

    video#8 revision on class based component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Header from "./components/Header"
+import Footer from "./components/Footer" 
+import "./App.css"
+import myImg from "./assets/pocLogo_black.png"
+import List from "./components/List"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+  products : [
+    {id : 1, title: "item1"},
+    {id : 2, title: "item2"},
+    {id : 3, title: "item2"},
+    {id : 4, title: "item2"},
+    {id : 5, title: "item2"}
+  ]
+  }
+  render() {
+    console.log(this.state);
+    return <div className="App">
+      <img src={myImg}/>
+          {this.state.desc}
+          <Header title="header comp2" desc="this uis header desc by me"  newData={this.state.desc}  />
+          <List products={this.state.products} />
+           <Footer title="footer comp"  />
+      </div>;
+  }
 }
 
 export default App;
