d23714d9b9914490ac38d0d0e9135b5e10914c27
dev-mahmoudwahba <devmahmoudwahba@gmail.com>
2022-03-07 02:47:33 +0200

    Axios get , post request

--- src/App.js
+++ src/App.js
@@ -6,12 +6,52 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Post from "./components/Post";
 import PostList from "./components/PostList";
-
+import axios from "axios"
 export default class App extends Component {
+  state = {
+    // name : "app",
+    // posts : []
+    userid : "",
+    title : "",
+    body : ""
+
+  }
+
+  // componentDidMount() {
+  //     axios.get("https://jsonplaceholder.typicode.com/posts")
+  //     .then(res=> this.setState({
+  //       posts : res.data
+  //     }))
+  //     .catch(err=> console.log(err))
+  // }
+  changeData = (e)=>{
+    this.setState({
+      [e.target.name] : e.target.value
+    })
+  }
+  submitData  = (e)=>{
+    e.preventDefault()
+    console.log(this.state);
+    axios.post("https://jsonplaceholder.typicode.com/posts",this.state)
+    .then(res=> console.log(res))
+  }
+
   render() {
     return (
       <div>
         <BrowserRouter>
+
+      <form onSubmit={this.submitData}>
+        <input name="userid" type="text" onChange={this.changeData} />
+        <input name="title" type="text"  onChange={this.changeData}/>
+        <input name="body" type="text" onChange={this.changeData} />
+        <input type="submit" />
+      </form>
+
+
+        {/* {
+          this.state.posts.length ? this.state.posts.map(post=> <div> {post.id } </div>  )  : ""
+        } */}
         <Navbar/> 
         <Route path="/" exact component={Home}  />
         <Route path="/compa" exact component={CompA} />
